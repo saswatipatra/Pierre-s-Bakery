2c3a71b9a3cb630c693fc9a0755c5d664049667d
saswati <p403saswati@gmail.com>
2019-07-19 10:54:48 -0700

    add bread and pasty class and it will take input from the user for no. of bread and no. of pastry

--- Models/Bakery.cs
+++ Models/Bakery.cs
@@ -1,8 +1,40 @@
 using System;
 namespace PierreBakery.Models
 {
-  class Bakery
+  class Bread
   {
+    private string _breadName;
+    private int _breadPrice;
+
+    public Bread(string breadName, int breadPrice)
+    {
+      _breadName = breadName;
+      _breadPrice = breadPrice;
+    }
+    public static void Bill(int bread, int pastry)
+    {
+      int bill=0;
+      if (bread!=0)
+      {
+        bill= bill + (bread*5);
+      }
+      if (pastry!=0)
+      {
+        bill= bill + (pastry*2);
+      }
+      Console.WriteLine("your total bill is: "+ bill);
+    }
+  }
+  class Pastry
+  {
+    private string _pastryName;
+    private int _pastryPrice;
+
+    public Pastry(string pastryName, int pastryPrice)
+    {
+      _pastryName = pastryName;
+      _pastryPrice = pastryPrice;
+    }
     public static void Bill(int bread, int pastry)
     {
       int bill=0;
--- program.cs
+++ program.cs
@@ -7,13 +7,33 @@ namespace PierreBakery
     public static void Main()
     {
       Console.WriteLine("Welcom to Pierre Bakery, Pierre offers the following deals: ");
-      Console.WriteLine("\u25C9" + " Bread: Buy 2, get 1 free. A single loaf costs $5.");
-      Console.WriteLine("\u25C9" + "Pastry: Buy 1 for $2 or 3 for $5.");
-      Console.WriteLine("Enter no. of Bread Loaf you want: ");
-      int breadNum= int.Parse(Console.ReadLine());
-      Console.WriteLine("Enter no. of Pastry you want: ");
-      int pastryNum= int.Parse(Console.ReadLine());
+      Console.WriteLine("\u25A0 Pastry: Buy 1 for $2 or 3 for $5.");
+      Console.WriteLine("Would you like to buy some Bread Loaf, enter yes/Y/y for yes: ");
+      int userBread= Console.ReadLine();
+      Console.WriteLine("Would you like to buy some Pastry, enter yes/Y/y for yes: ");
+      int userBread= Console.ReadLine();
+      // int userPastry= int.Parse(Console.ReadLine());
+      if (userBread=="yes" || userBread=="Yes"|| userBread=="y"|| userBread=="Y")
+      {
+        Console.WriteLine("\u25A0 Bread: Buy any 2, get 1 free. A single loaf costs $5.");
+        Console.WriteLine("Below is the list of bread loaf we have:");
+        Bread bread1 = new Bread("Whole-Wheat", 5);
+        Bread bread2 = new Bread("Sourdough", 5);
+        Bread bread3 = new Bread("Rye", 5);
+        Bread bread4 = new Bread("Multigrain", 5);
+        List<Bread> Breads = new List<Bread>() { bread1, bread2, bread3, bread4 };
+        foreach(Bread allbread in Breads)
+        {
+          Console.WriteLine("----------------------");
+          Console.WriteLine(allbread.GetName());
+        }
+
+        Bakery.bread(call...)
+      }
+      if (userPastry=="yes" || userPastry=="Yes"|| userPastry=="y"|| userPastry=="Y")
+      {
       Bakery.Bill(breadNum,pastryNum);
+      }
     }
   }
 }
