b7d9482919ce7a1da73ab295760660a88ccf29ca
saswati <p403saswati@gmail.com>
2019-07-19 09:11:03 -0700

    add initinal files

---
+++ program.cs
@@ -0,0 +1,11 @@
+using System;
+using PierreBakery.Models;
+namespace PierreBakery
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcom to Pierre Bakery, Pierre offers the following deals: ")
+      Console.WriteLine("\u25C9 Bread: Buy 2, get 1 free. A single loaf costs $5.");
+      Console.WriteLine("\u25C9 Pastry: Buy 1 for \$2 or 3 for $5.");
