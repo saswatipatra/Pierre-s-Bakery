80260ef7a0eb5d33c868c867f93b18f648a3c600
saswati <p403saswati@gmail.com>
2019-07-21 10:54:49 -0700

    add different types of bread with different prices

--- Models/Bakery.cs
+++ Models/Bakery.cs
@@ -16,11 +16,33 @@ namespace PierreBakery.Models
     {
       return _breadName;
     }
-    public static int BreadBill(int breadNum)
+    public int GetBreadPrice()
+    {
+      return _breadPrice;
+    }
+    public static int BreadBill(int breadNum, string name)
     {
       int bill=0;
-      bill= bill + (breadNum*5);
-      return bill;
+      if(name=="Whole-Wheat" || name=="whole-wheat")
+      {
+        bill= bill + (breadNum*5);
+        return bill;
+      }
+      else if (name=="Rye"||name=="rye")
+      {
+        bill= bill + (breadNum*6);
+        return bill;
+      }
+      else if (name=="Multigrain"||name=="multigrain")
+      {
+        bill= bill + (breadNum*7);
+        return bill;
+      }
+      else
+      {
+        bill= bill + (breadNum*4);
+        return bill;
+      }
     }
   }
   class Pastry
@@ -40,8 +62,23 @@ namespace PierreBakery.Models
     public static int PastryBill(int pastry)
     {
       int pastryBill=0;
-      pastryBill= pastryBill + (pastry*2);
-      return pastryBill;
+      if (pastry%3==0){
+        int cost= pastry/3;
+        pastryBill= pastryBill + (cost*5);
+        return pastryBill;
+      }
+      else if (pastry==1)
+      {
+        pastryBill= pastryBill + (pastry*2);
+        return pastryBill;
+      }
+      else
+      {
+        int three=pastry/3;
+        int remaining= (pastry-(three*3));
+        pastryBill= pastryBill + (three*5) + (remaining*2);
+        return pastryBill;
+      }
     }
   }
 }
--- program.cs
+++ program.cs
@@ -14,26 +14,38 @@ namespace PierreBakery
       string userPastry= Console.ReadLine();
       if (userBread=="yes" || userBread=="Yes"|| userBread=="y"|| userBread=="Y")
       {
-        Console.WriteLine("\u25A0 Bread: Buy any 2, get 1 free. A single loaf costs $5.");
         Console.WriteLine("Below is the list of bread loaf we have:");
         Bread bread1 = new Bread("Whole-Wheat", 5);
-        Bread bread2 = new Bread("Sourdough", 5);
-        Bread bread3 = new Bread("Rye", 5);
-        Bread bread4 = new Bread("Multigrain", 5);
+        Bread bread2 = new Bread("Sourdough", 4);
+        Bread bread3 = new Bread("Rye", 6);
+        Bread bread4 = new Bread("Multigrain", 7);
         List<Bread> Breads = new List<Bread>() { bread1, bread2, bread3, bread4 };
         foreach(Bread allbread in Breads)
         {
           Console.WriteLine("----------------------");
           Console.WriteLine(allbread.GetBreadName());
+          Console.WriteLine(allbread.GetBreadPrice() + "$");
         }
-        Console.WriteLine("Enter no. of bread loaf you want: ");
-        int numBread= int.Parse(Console.ReadLine());
-        if (numBread!= 0)
+        string flavorName="";
+        int quantity=0;
+        int totalBill=0;
+        string userWish="yes";
+        while(userWish=="yes"||userWish=="Yes"|| userWish=="y"||userWish=="Y")
         {
-          int breadAmt= Bread.BreadBill(numBread);
-          Console.WriteLine("No. of bread you order: " + numBread + " your bill: " + breadAmt + "$");
+          Console.WriteLine("Enter the Name: ");
+          flavorName= Console.ReadLine();
+          Console.WriteLine("Enter how many you want: ");
+          quantity= int.Parse(Console.ReadLine());
+          if (quantity!= 0)
+          {
+            int bill=Bread.BreadBill(quantity, flavorName);
+            totalBill= totalBill+bill;
+          }
+          Console.WriteLine("would you like to order more breads enter ");
+          userWish=Console.ReadLine();
         }
-      }
+        Console.WriteLine("your total bill is: " + totalBill + "$");
+    }
       if (userPastry=="yes" || userPastry=="Yes"|| userPastry=="y"|| userPastry=="Y")
       {
         Console.WriteLine("\u25A0 Pastry: Buy 1 for $2 or 3 for $5.");
@@ -57,5 +69,6 @@ namespace PierreBakery
         }
       }
     }
+
   }
 }
